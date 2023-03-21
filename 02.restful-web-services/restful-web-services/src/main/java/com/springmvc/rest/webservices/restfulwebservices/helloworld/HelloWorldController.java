3a6d344c83713d36dc99f9ea0b62b58566024608
thisismemukul <w41k3r.12@gmail.com>
2023-03-21 19:30:55 +0530

    Step 03 - Enhancing the Hello World REST API to return a Bean

--- 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,8 +1,6 @@
 package com.springmvc.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //RESRT
@@ -14,6 +12,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
 	
 }
