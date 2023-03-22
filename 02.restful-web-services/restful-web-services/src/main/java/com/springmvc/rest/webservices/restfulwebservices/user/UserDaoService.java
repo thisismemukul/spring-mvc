1a6bc28e4ea81c0f8faea6451a9a367a1e24f6d2
thisismemukul <w41k3r.12@gmail.com>
2023-03-22 12:21:48 +0530

    Step 09 - Implementing POST Method to create User Resource

--- 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -15,11 +15,12 @@ public class UserDaoService {
 	
 	//public List<User> findAll(){
 	public static List<User> users = new ArrayList<>();
+	private static int usersCount =0; 
 	static {
-		users.add(new User(1,"Adam",LocalDate.now().minusYears(30)));
-		users.add(new User(2,"Mukul",LocalDate.now().minusYears(22)));
-		users.add(new User(3,"Eve",LocalDate.now().minusYears(25)));
-		users.add(new User(4,"Jim",LocalDate.now().minusYears(20)));
+		users.add(new User(++usersCount,"Adam",LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount,"Mukul",LocalDate.now().minusYears(22)));
+		users.add(new User(++usersCount,"Eve",LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount,"Jim",LocalDate.now().minusYears(20)));
 	}
 	
 
@@ -27,6 +28,11 @@ public class UserDaoService {
 		return users;
 	}
 	//public User save(User user){	
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
 	//public User findOne(int id){
 	
 	public User findOne(int id) {
