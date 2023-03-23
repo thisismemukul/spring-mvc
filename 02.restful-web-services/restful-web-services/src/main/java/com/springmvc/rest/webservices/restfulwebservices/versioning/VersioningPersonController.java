05c105b73eb552520babec7737ef4db8e9c6f1e8
thisismemukul <w41k3r.12@gmail.com>
2023-03-23 13:58:50 +0530

    Step 21 - Versioning REST API - Request Param, Header and Content Negotiation

--- 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -13,4 +13,28 @@ public class VersioningPersonController {
 	public PersonV2 getSecondVersionofPerson() {
 		return new PersonV2(new Name("Mukul","Saini"));
 	}
+	@GetMapping(path="/person",params = "version=1")
+	public PersonV1 getFirstVersionofPersonRequestParameter() {
+		return new PersonV1("Mukul Saini");
+	}
+	@GetMapping(path="/person",params = "version=2")
+	public PersonV2 getSecondVersionofPersonRequestParameter() {
+		return new PersonV2(new Name("Mukul","Saini"));
+	}
+	@GetMapping(path="/person/header",headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionofPersonRequestHeader() {
+		return new PersonV1("Mukul Saini");
+	}
+	@GetMapping(path="/person/header",headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionofPersonRequestHeader() {
+		return new PersonV2(new Name("Mukul","Saini"));
+	}
+	@GetMapping(path="/person/accept",produces = "application/vdn.company.app-v1+json")
+	public PersonV1 getFirstVersionofPersonAccepttHeader() {
+		return new PersonV1("Mukul Saini");
+	}
+	@GetMapping(path="/person/accept",produces = "application/vdn.company.app-v2+json")
+	public PersonV2 getSecondVersionofPersonAccepttHeader() {
+		return new PersonV2(new Name("Mukul","Saini"));
+	}
 }
