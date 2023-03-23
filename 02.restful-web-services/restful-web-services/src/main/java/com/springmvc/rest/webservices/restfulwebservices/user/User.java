0fa36bec5f17a0a1f511b12212b5247775c65fff
thisismemukul <w41k3r.12@gmail.com>
2023-03-23 16:21:25 +0530

    Step 23 - Implementing Static Filtering for REST API

--- 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/user/User.java
+++ 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/user/User.java
@@ -2,6 +2,8 @@ package com.springmvc.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,8 +11,10 @@ public class User {
 
 	private Integer id;
 	@Size(min = 2, message = "Name should have atleast 2 characters")
+	@JsonProperty("user_name")
 	private String name;
 	@Past(message = "Birthdate should be in the past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
