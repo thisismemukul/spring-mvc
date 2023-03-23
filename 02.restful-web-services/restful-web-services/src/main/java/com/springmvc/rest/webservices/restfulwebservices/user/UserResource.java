129d662ba723f803b4d4b3c3415b745d44043d19
thisismemukul <w41k3r.12@gmail.com>
2023-03-23 15:52:07 +0530

    Step 22 - Implementing HATEOAS for REST API

--- 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/user/UserResource.java
+++ 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,13 @@
 package com.springmvc.rest.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,13 +30,22 @@ public class UserResource {
 	public List<User> retriveAllUsers() {
 		return service.findAll();
 	}
+	
+	//http://localost:8080/users
+	
+	//EntityModel
+	//WebMvcLinkBuilder
+	
 	//GET/users/:id
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
 		if(user == null)
 			throw new UserNotFoundException("id:"+id);
-		return user;
+		EntityModel<User> entityModel = EntityModel.of(user);
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retriveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		return entityModel;
 	}
 	//DELETE/users/:id
 		@DeleteMapping("/users/{id}")
