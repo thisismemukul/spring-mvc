6af1b2bc6734ead9ac7c8a2454ef798b9afedfa3
thisismemukul <w41k3r.12@gmail.com>
2023-03-23 16:44:55 +0530

    Step 24 - Implementing Dynamic Filtering for REST API

--- 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -3,17 +3,28 @@ package com.springmvc.rest.webservices.restfulwebservices.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.hibernate.annotations.DialectOverride.Filters;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 	
-	@GetMapping("/filtering")
-	public SomeBean filtering() {
-		return new SomeBean("value1","value2","value3");
+	@GetMapping("/filtering") //field 2
+	public MappingJacksonValue filtering() {
+		SomeBean someBean = new SomeBean("value1","value2","value3");
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("filed1","field3");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		mappingJacksonValue.setFilters(filters);
+		return mappingJacksonValue;
 	}
-	@GetMapping("/filtering-list")
+	@GetMapping("/filtering-list") //field 1
 	public List<SomeBean> filteringList() {
 		return Arrays.asList(new SomeBean("value1","value2","value3"),new SomeBean("value4","value5","value6"));
 	}
--- 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,9 +1,11 @@
 package com.springmvc.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties({"field1","field2"})
+//@JsonIgnoreProperties({"field1","field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 	private String field1;
 //	@JsonIgnore
