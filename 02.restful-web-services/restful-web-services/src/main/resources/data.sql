ca3af1d962ada09e6f8397e7688b216b5ee502ae
thisismemukul <w41k3r.12@gmail.com>
2023-03-24 11:32:38 +0530

    Step 28 - Creating User Entity and some test data

---
+++ 02.restful-web-services/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+--create table person
+--(
+--   id integer not null,
+--   name varchar(255) not null,
+--   location varchar(255),
+--   primary key(id)
+--);
+
+INSERT INTO user_details (ID, BIRTH_DATE, NAME) 
+VALUES(10001,  current_date(), 'Ranga');
+INSERT INTO user_details (ID, BIRTH_DATE, NAME) 
+VALUES(10002,  current_date(), 'Ranga 1');
+INSERT INTO user_details (ID, BIRTH_DATE, NAME) 
+VALUES(10003,  current_date(), 'Ranga 2');
