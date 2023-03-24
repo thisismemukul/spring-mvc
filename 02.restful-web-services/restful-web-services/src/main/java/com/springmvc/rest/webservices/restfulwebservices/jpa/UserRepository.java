399f965da73d8106bfbb1a806583147be1681cfb
thisismemukul <w41k3r.12@gmail.com>
2023-03-24 19:16:36 +0530

    Step 01 - Setting up Limits Microservice - V2

--- 02.restful-web-services/restful-web-services/src/main/java/com/springmvc/rest/webservices/restfulwebservices/jpa/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springmvc.rest.webservices.restfulwebservices.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springmvc.rest.webservices.restfulwebservices.user.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
