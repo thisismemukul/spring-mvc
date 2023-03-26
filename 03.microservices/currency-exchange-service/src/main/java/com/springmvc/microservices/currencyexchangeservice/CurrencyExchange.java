980c0c590feb61a9c3e4da7213e7d017558cafe3
thisismemukul <w41k3r.12@gmail.com>
2023-03-26 19:08:35 +0530

    Step 13 - Configure JPA and Initialized Data - V2

--- 03.microservices/currency-exchange-service/src/main/java/com/springmvc/microservices/currencyexchangeservice/CurrencyExchange.java
+++ 03.microservices/currency-exchange-service/src/main/java/com/springmvc/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,10 +1,18 @@
 package com.springmvc.microservices.currencyexchangeservice;
 import java.math.BigDecimal;
 
-public class CurrencyExchange {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class CurrencyExchange {
+	
+	@Id
     private Long id;
+	@Column(name="currency_form")
     private String from;
+	@Column(name="currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
